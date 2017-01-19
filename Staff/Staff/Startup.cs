8e8015d27e1bf29c3e112fae2419ac96a08bfb61
Павел Костин < >
2017-01-19 21:39:32 +0200

    created project

---
+++ Staff/Staff/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Staff.Startup))]
+namespace Staff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
